77211849f0aed8f63be82a314ee51bba29672b1d
Per Nordlöw <per.nordlow@gmail.com>
2014-08-13 19:02:58 +0200

    Ups

--- others/icicles-chg.el
+++ others/icicles-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2007-2014, Drew Adams, all rights reserved.
 ;; Created: Tue Nov 27 07:47:53 2007
-;; Last-Updated: Mon Aug 11 10:25:27 2014 (-0700)
+;; Last-Updated: Tue Aug 12 16:54:35 2014 (-0700)
 ;;           By: dradams
-;;     Update #: 11132
+;;     Update #: 11135
 ;; URL: http://www.emacswiki.org/icicles-chg.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -1110,6 +1110,8 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-cmd2.el'")
 ;;
+;; 2014/08/12 dadams
+;;     Added: icicle-wide-n, icicle-wide-n-action.
 ;; 2014/08/10 dadams
 ;;     Added: icicle-describe-package.
 ;;     Moved to icicles-fn.el (and modified): icicle-defined-thing-p.
@@ -2058,6 +2060,8 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-fn.el'")
 ;;
+;; 2014/08/12 dadams
+;;     icicle-special-candidates-first-p: Use icicle-special-candidate-p.
 ;; 2014/08/10 dadams
 ;;     Added:
 ;;       icicle-bookmark-autofile-p, icicle-bookmark-autonamed-p,
