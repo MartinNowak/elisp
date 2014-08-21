11f6cc5758ac51512c2d378194abe87cabd4c05f
Per Nordlöw <per.nordlow@gmail.com>
2014-08-22 00:39:01 +0200

    Fix

--- mine/dotemacs.el
+++ mine/dotemacs.el
@@ -1534,6 +1534,8 @@ save it in `ffap-file-at-point-line-number' variable."
 (when (append-to-load-path (elsub "asciidoc-mode"))
   (autoload 'asciidoc-mode "asciidoc-mode" "Major mode for editing D code." t)
   (add-to-list 'auto-mode-alist '("\\.\\(asciidoc\\)" . asciidoc-mode))
+  (add-to-list 'auto-mode-alist '("\\.\\(ad\\)" . asciidoc-mode))
+  (add-to-list 'auto-mode-alist '("\\.\\(adoc\\)" . asciidoc-mode))
   )
 
 ;;; ===========================================================================
