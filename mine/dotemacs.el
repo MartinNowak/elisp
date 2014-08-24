deb6638b237c3aa55489f6a124208538d226d207
Per Nordlöw <per.nordlow@gmail.com>
2014-08-24 16:10:09 +0200

    Fix loading of neotree

--- mine/dotemacs.el
+++ mine/dotemacs.el
@@ -868,9 +868,7 @@
                       'async-shell-command
                     'background))
   ;; BASH completion for the shell buffer
-  (autoload 'bash-completion-dynamic-complete
-    "bash-completion"
-    "BASH completion hook")
+  (autoload 'bash-completion-dynamic-complete "bash-completion" "BASH completion hook")
   (add-hook 'shell-dynamic-complete-functions
             'bash-completion-dynamic-complete)
   (add-hook 'shell-command-complete-functions
