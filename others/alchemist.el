8e793593fdfdae675b268abaa5ac82d7cd4b4dde
Per Nordlöw <per.nordlow@gmail.com>
2014-11-14 23:44:14 +0100

    Fixes

---
+++ others/alchemist.el
@@ -0,0 +1,6 @@
+[http://elixir-lang.org/ Elixir] Tooling Integration Into Emacs
+
+More information and installation instructions are available on [http://github.com/tonini/alchemist.el GitHub].
+
+----
+ElixirLanguage ProgrammingModes SamuelTonini
