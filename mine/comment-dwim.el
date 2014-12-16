276355fe55a107f64e44d27d81ae265f5e12db46
Per Nordlöw <per.nordlow@gmail.com>
2014-12-16 21:22:02 +0100

    Fix

--- mine/comment-dwim.el
+++ mine/comment-dwim.el
@@ -4,6 +4,7 @@
 (require 'paredit)
 (require 'newcomment)
 (require 'cc-assist)
+(require 'rx)
 
 (defvar comment-dwim-repeats 0
   "Repeat counter for `comment-dwim-really'.")
@@ -62,6 +63,45 @@ c++-mode, java-mode and csharp-mode."
           (t
            (comment-dwim arg)
            ))))
-(global-set-key [(meta \;)] 'comment-dwim-really)
+
+;; TODO Call c-insert-doxygen-line-comment if we are standing after a variable declaration/definition.
+(defun c-end-of-doxygen-line-comment ()
+  "Go to end of enhanced (Doxygen) line comments."
+  (when (cc-derived-mode-p)
+    (unless (looking-back (rx space))
+      (re-search-forward (concat "/*\\**" "[<!]*"
+                                 "\\("
+                                 _*
+                                 "\\_<" ;text already present
+                                 "\\|"
+                                 " "
+                                 "\\)")))))
+
+(defadvice comment-dwim (after end-of-c-doxygen-line activate)
+  "Adjust cursor to correct position of enhanced comments."
+  (c-end-of-doxygen-line-comment))
+(ad-deactivate 'comment-dwim)
+
+(defun c-adjust-comments ()
+  (setq comment-start "/*"
+        comment-end "*/"))
+
+(add-hook 'c-mode-hook 'c-adjust-comments)
+(add-hook 'c++-mode-hook 'c-adjust-comments)
+(add-hook 'd-mode-hook 'c-adjust-comments)
+(add-hook 'flex-mode-hook 'c-adjust-comments)
+
+(add-hook 'lisp-mode-hook (lambda () (local-set-key [(control c) (\;)] 'comment-region)))
+(add-hook 'emacs-lisp-mode-hook (lambda () (local-set-key [(control c) (meta \;)] 'comment-region)))
+(add-hook 'sh-mode-hook (lambda ()
+                          ;; override shell mode case statement template insertion
+                          (local-set-key [(control c) (\;)] 'comment-region)
+                          (local-set-key [(control c) (meta \;)] 'uncomment-region)
+                          ))
+
+;;(eload 'c-comment-edit)             ;Edit C comments
+(global-set-key [(control c) (\;)] 'comment-region)
+(global-set-key [(control c) (meta \;)] 'uncomment-region)
+(global-set-key [(meta \;)] 'comment-dwim)
 
 (provide 'comment-dwim)
